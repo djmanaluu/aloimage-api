192726f8b2c0f28b8caf6cf90bda8f5585a93b04
David Jourdan Manalu <david.jourdan@traveloka.com>
2020-11-26 12:24:52 +0700

    Configure heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -14,14 +14,23 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self)
     services.register(middlewares)
-
-    let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
-        username: "vapor_username",
-        database: "vapor_database",
-        password: "vapor_password")
+    
+    let databaseConfig: PostgreSQLDatabaseConfig
+    
+    if let url = Environment.get("DATABASE_URL") {
+      databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    }
+    else {
+        let databaseConfig = PostgreSQLDatabaseConfig(
+            hostname: Environment.get("DATABASE_HOSTNAME") ?? "localhost",
+            username: "vapor_username",
+            database: "vapor_database",
+            password: "vapor_password")
+        
+    }
+    
     let database = PostgreSQLDatabase(config: databaseConfig)
-
+    
     var databases = DatabasesConfig()
     databases.add(database: database, as: .psql)
     services.register(databases)
